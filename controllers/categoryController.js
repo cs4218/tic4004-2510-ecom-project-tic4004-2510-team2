53cf9c1c0a927cf01575616b5b03269147a9a37a
bowenENTIIS <bowen@entiis.com>
2025-10-27 23:06:48 +0800

    update wrong use of error code for createCategory response. 401 should be `unautheorized`

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -4,7 +4,7 @@ export const createCategoryController = async (req, res) => {
   try {
     const { name } = req.body;
     if (!name) {
-      return res.status(401).send({ message: "Name is required" });
+      return res.status(400).send({ message: "Name is required" });
     }
     const existingCategory = await categoryModel.findOne({ name });
     if (existingCategory) {
